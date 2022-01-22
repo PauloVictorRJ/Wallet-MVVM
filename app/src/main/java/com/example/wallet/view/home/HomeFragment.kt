f8658e81a4ca43a37d5b5b2460143784a1fe71c4
Paulo Victor <c4.paulovictor@gmail.com>
2022-01-22 09:03:23 -0800

    ViewBinding implementado

--- app/src/main/java/com/example/wallet/view/home/HomeFragment.kt
+++ app/src/main/java/com/example/wallet/view/home/HomeFragment.kt
@@ -1,7 +1,9 @@
 package com.example.wallet.view.home
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -9,6 +11,7 @@ import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.ViewPager2
 import com.digitalhouse.dhwallet.util.decorator.HorizontalMarginItemDecoration
 import com.example.wallet.R
+import com.example.wallet.databinding.FragmentHomeBinding
 import com.example.wallet.repositories.CardsRepository
 import com.example.wallet.repositories.TransactionsRepository
 import com.example.wallet.util.CustomPageTransformer
@@ -17,12 +20,21 @@ import com.example.wallet.viewmodel.HomeViewModelFactory
 
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding: FragmentHomeBinding get() = _binding!!
+
     private lateinit var viewModel: HomeViewModel
+
     private val transactionAdapter = HomeRvTransactionsAdapater()
     private lateinit var cardsAdapter: HomeCardAdapter
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        cardsAdapter = HomeCardAdapter(this)
+
         viewModel = ViewModelProvider(
             this,
             HomeViewModelFactory(TransactionsRepository(), CardsRepository())
@@ -33,22 +45,19 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         viewModel.requestTransactions()
         viewModel.requestCards()
 
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val viewPager2: ViewPager2 = view.findViewById(R.id.list_card)
-        cardsAdapter = HomeCardAdapter(this)
+        val viewPager2 = binding.listCard
         viewPager2.adapter = cardsAdapter
-
         viewPager2.setPageTransformer(CustomPageTransformer(view.context))
         viewPager2.offscreenPageLimit = 1
-
         viewPager2.addItemDecoration(
             HorizontalMarginItemDecoration(
-                view.context,
-                R.dimen.viewpager_current_item_horizontal_margin
+                view.context, R.dimen.viewpager_current_item_horizontal_margin
             )
         )
 
@@ -56,11 +65,16 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
             cardsAdapter.setListCards(it)
         })
 
-        val rvHomeTransacoes: RecyclerView = view.findViewById(R.id.rv_home_transacoes)
+        val rvHomeTransacoes = binding.rvHomeTransacoes
         rvHomeTransacoes.adapter = transactionAdapter
 
         viewModel.liveListTransactions.observe(viewLifecycleOwner, Observer {
             transactionAdapter.setListaTransaction(it)
         })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
